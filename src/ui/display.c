29082c1205f71cd0acc6be96c0fe7e72ca21c0a9
Charles Madjeri <studies@madjeri.com>
2023-04-17 21:44:55 +0200

    [ADD]: map config parser, map builder from textures and  map full sprite builder.

--- src/ui/display.c
+++ src/ui/display.c
@@ -8,8 +8,15 @@
 #include "../../include/main.h"
 #include "../../include/game.h"
 
+void display_map(sfRenderWindow *window, map *map)
+{
+    sfRenderWindow_drawSprite(window, map->map_sprite, NULL);
+    return;
+}
+
 void display(sfRenderWindow *window, game *game)
 {
-        display_player(window, game->player);
+    display_map(window, game->map);
+    display_player(window, game->player);
     return;
 }
